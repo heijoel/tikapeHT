ac057c374a3919b35f0055033cafa41ed96d4931
heijoel <64308226+heijoel@users.noreply.github.com>
2020-06-05 01:12:46 +0300

    Näillä näkymin valmis harjoitustyön 1.tehtävä. Toimii kuten esimerkkitulostus MOOC-sivulla

--- src/tikapeht1/TikapeHT1.java
+++ src/tikapeht1/TikapeHT1.java
@@ -1,11 +1,9 @@
-
 package tikapeht1;
 
 /**
  *
- * @author joel
+ * @author joel.heino95@gmail.com
  */
-
 import java.sql.*;
 import java.util.*;
 
@@ -35,7 +33,6 @@ public class TikapeHT1 {
                 toiminto4();
                 System.out.println("");
             } else {
-                System.out.println("Ei kyseistä toimintoa. Ohjelma loppuu.");
                 break;
             }
         }
@@ -57,14 +54,15 @@ public class TikapeHT1 {
 
             ResultSet r = p.executeQuery();
 
+            //if (r.next() && r.getString("summa")!= null) {    //halutaanko kirjallinen ilmoitus vai vaan 0?
             if (r.next()) {
-                System.out.println("Opintopisteiden määrä: " + r.getString("summa"));
+                System.out.println("Opintopisteiden määrä: " + r.getInt("summa"));
             } else {
                 System.out.println("Annetulta vuodelta ei ole suorituksia tietokannassa");
             }
 
         } catch (SQLException e) {
-            System.out.println("Annetulta vuodelta ei ole suorituksia tietokannassa");
+            System.out.println("Tietokantahaku ei onnistunut");
         }
 
     }
@@ -77,20 +75,31 @@ public class TikapeHT1 {
         String opiskelija = input.nextLine();
 
         try {
-            PreparedStatement p = db.prepareStatement("SELECT K.nimi, K.laajuus, S.paivays, S.arvosana\n"
-                    + "FROM Opiskelijat O, Kurssit K, Suoritukset S\n"
-                    + "WHERE O.id = S.opiskelija_id AND K.id = S.kurssi_id AND O.nimi = ? ORDER BY S.paivays");
-            p.setString(1, opiskelija);
+            PreparedStatement p1 = db.prepareStatement("SELECT COUNT(*) AS opiskelijaTietokannassa FROM Opiskelijat WHERE nimi = ?");
+            p1.setString(1, opiskelija);
+            ResultSet r1 = p1.executeQuery();
 
-            ResultSet r = p.executeQuery();
+            if (r1.getInt("opiskelijaTietokannassa") < 1) {
+                System.out.println("Opiskelijaa ei löytynyt");
+            } else {
 
-            while (r.next()) {
-                System.out.println(r.getString("nimi") + " " + r.getInt("laajuus") + " "
-                        + r.getString("paivays") + " " + r.getInt("arvosana"));
+                PreparedStatement p2 = db.prepareStatement("SELECT K.nimi, K.laajuus, S.paivays, S.arvosana\n"
+                        + "FROM Opiskelijat O, Kurssit K, Suoritukset S\n"
+                        + "WHERE O.id = S.opiskelija_id AND K.id = S.kurssi_id AND O.nimi = ? ORDER BY S.paivays");
+                p2.setString(1, opiskelija);
+
+                ResultSet r2 = p2.executeQuery();
+
+                //if (r.next() == false) {
+                //   System.out.println("Opiskelijaa ei löytynyt");
+                //}
+                while (r2.next()) {
+                    System.out.println(r2.getString("nimi") + " " + r2.getInt("laajuus") + " "
+                            + r2.getString("paivays") + " " + r2.getInt("arvosana"));
+                }
             }
-
         } catch (SQLException e) {
-            System.out.println("Opiskelijaa ei löytynyt");
+            System.out.println("Tietokantahaku ei onnistunut");
         }
 
     }
@@ -103,18 +112,27 @@ public class TikapeHT1 {
         String kurssi = input.nextLine();
 
         try {
-            PreparedStatement p = db.prepareStatement(
-                    "SELECT ROUND(1.0*SUM(S.arvosana)/COUNT(S.arvosana),2) AS keskiarvo"
-                    + " FROM Kurssit K, Suoritukset S "
-                    + "WHERE K.id=S.kurssi_id AND K.nimi = ?");
-            p.setString(1, kurssi);
 
-            ResultSet r = p.executeQuery();
+            //Tässä tarkistetaan, onko käyttäjän antama syöte tietokannassa oleva kurssi.
+            //Mikäli syötettyä kurssia ei löydy tietokannasta, tulostetaan tästä tieto käyttäjälle
+            //Mikäli kurssi löytyy, siirrytään ohjelman osaan, jossa tehdään syötetyn kurssin
+            //keskiarvon laskeva tietokantakysely.
+            PreparedStatement p1 = db.prepareStatement("SELECT COUNT(*) AS syoteListalla FROM Kurssit WHERE nimi = ?");
+            p1.setString(1, kurssi);
+            ResultSet r1 = p1.executeQuery();
 
-            if (r.next()) {
-                System.out.println("Keskiarvo: " + r.getString("keskiarvo"));
+            if (r1.getInt("syoteListalla") < 1) {
+                System.out.println("Kurssia ei löytynyt");
             } else {
-                System.out.println("Keskiarvoa ei pystytty laskemaan");
+                PreparedStatement p2 = db.prepareStatement(
+                        "SELECT ROUND(1.0*SUM(S.arvosana)/COUNT(S.arvosana),2) AS keskiarvo"
+                        + " FROM Kurssit K, Suoritukset S "
+                        + "WHERE K.id=S.kurssi_id AND K.nimi = ?");
+                p2.setString(1, kurssi);
+                ResultSet r = p2.executeQuery();
+                if (r.next()) {
+                    System.out.println("Keskiarvo: " + r.getString("keskiarvo"));
+                }
             }
 
         } catch (SQLException e) {
@@ -127,34 +145,42 @@ public class TikapeHT1 {
         Scanner input = new Scanner(System.in);
         Connection db = DriverManager.getConnection("jdbc:sqlite:kurssit.db");
 
-        int lkm=0;
+        //Pyydetään käyttäjältä tulostettavien opettajien lukumäärää. Jos lkm < 1, pyydetään
+        //syöttämään sellainen lukumäärä (yksi tai suurempi), jotta toiminnon mukainen
+        //tulostus olisi mielekäs.
+        int lkm = 0;
         while (true) {
             System.out.print("Anna opettajien määrä: ");
             lkm = Integer.valueOf(input.nextLine());
             if (lkm > 0) {
                 break;
             } else {
-                System.out.println("Anna opettajien määrä positiivisena kokonaislukuna.");
+                System.out.println("Opettajien määrän pitää olla yksi tai suurempi");
             }
         }
 
         try {
             PreparedStatement p = db.prepareStatement(
-                    "SELECT O.nimi, SUM(K.laajuus) AS summa FROM Opettajat O, Suoritukset S, Kurssit K "
+                    "SELECT O.nimi AS opettaja, SUM(K.laajuus) AS op FROM Opettajat O, Suoritukset S, Kurssit K "
                     + "WHERE O.id = K.opettaja_id AND S.kurssi_id = K.id GROUP BY O.id "
                     + "ORDER BY SUM(K.laajuus) DESC LIMIT ?");
             p.setInt(1, lkm);
 
             ResultSet r = p.executeQuery();
-
+            
+            //Tulostetaan top-listalle otsikot
+            if (r.next()) {
+                ResultSetMetaData rsmd = r.getMetaData();
+                System.out.println(rsmd.getColumnName(1) + " " + rsmd.getColumnName(2));
+            }
+            
+            //Tulostetaan käyttäjän syöttämä määrä (x) eniten opintopisteitä antaneita opettajia
             while (r.next()) {
-                System.out.println(r.getString("nimi") + " " + r.getString("summa"));
+                System.out.println(r.getString("opettaja") + " " + r.getInt("op"));
             }
 
         } catch (SQLException e) {
-            System.out.println("Tietoa annetulle määrälle opettajia ei pystytty hakemaan");
+            System.out.println("Tietokantahaku ei onnistunut");
         }
-
     }
-
 }
